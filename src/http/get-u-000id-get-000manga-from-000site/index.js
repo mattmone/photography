d2cbdcdccfe04d05836fc4a0d36eb2462089d77f
Matthew Mone <mmone@vernier.com>
2020-08-09 13:52:51 -0700

    switch user usages for manga adding

--- src/http/get-u-000id-get-000manga-from-000site/index.js
+++ src/http/get-u-000id-get-000manga-from-000site/index.js
@@ -7,17 +7,9 @@ exports.handler = async function http (req) {
   const { id: userid, manga, site } = req.pathParameters;
   const userSettings = (await data.get({table: 'users', key: userid})) || {};
   const mangaRecord = (await data.get({table: 'manga', key: manga})) || {};
-  if((mangaRecord||{}).from === site) {
-    if(userSettings[manga]) return respondWith(`User ${userid} already has this title.`);
-    return respondWith(`We already have ${manga} in the database`);
-  } else if((mangaRecord||{}).from !== undefined ) {
-    const oldSite = mangaRecord.from;
-    mangaRecord.from = site;
-    await data.set({table: "manga", key: manga, data: JSON.stringify(mangaRecord)})
-    return respondWith(`Switched ${manga} from ${oldSite} to ${site}.`)
-  }
-  userSettings[manga] = {};
+
   mangaRecord.from = site;
+  mangaRecord.addedBy = mangaRecord.addedBy || userid;
   
   const chapters = await gin[site].chapters(manga);
   const chapterNumbers = chapters.map(chapter => Number(chapter.chap_number)).filter(chapterNumber => chapterNumber > (mangaRecord.lastChapter || -1));
@@ -30,11 +22,10 @@ exports.handler = async function http (req) {
     const chapterNumber = chapterNumbers[retrievedChapters.indexOf(chapter)];
     mangaRecord.chapters[chapterNumber] = imageUrls.map(i => i.src);
     mangaRecord.lastChapter = chapterNumber;
-    break;
   }
   const mangaError = await data.set({table: "manga", key: manga, data: JSON.stringify(mangaRecord)}).catch(err => err)
   const userError = await data.set({table: "users", key: userid, settings: JSON.stringify(userSettings)}).catch(err => err)
-  return respondWith(`done ${mangaError ? "manga error" : "manga success"} ${userError ? "user error" : "user success"}`);
+  return respondWith(`added ${manga} to the library. retrieved up to chapter ${mangaRecord.lastChapter}. attached it to user ${userid}.`);
 }
 
 function respondWith(msg) {
