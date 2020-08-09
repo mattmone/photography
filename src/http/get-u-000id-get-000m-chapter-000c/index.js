468d6cdd184f66aa767d1c10413890ace3fb8f73
Matthew Mone <mmone@vernier.com>
2020-08-09 14:44:14 -0700

    try generated

--- src/http/get-u-000id-get-000m-chapter-000c/index.js
+++ src/http/get-u-000id-get-000m-chapter-000c/index.js
@@ -3,19 +3,18 @@ const data = require('@begin/data')
 
 
 exports.handler = async function http (req) {
-  // const { id: userid, m: manga, c: chapter } = req.pathParameters;
-  // const userSettings = (await data.get({table: 'users', key: userid})) || {};
-  // const mangaRecord = (await data.get({table: 'manga', key: manga}));
-  // if(!mangaRecord) return respondWith(JSON.stringify({failure: `no manga found with name ${manga}`}));
+  const { id: userid, m: manga, c: chapter } = req.pathParameters;
+  const userSettings = (await data.get({table: 'users', key: userid})) || {};
+  const mangaRecord = (await data.get({table: 'manga', key: manga}));
+  if(!mangaRecord) return respondWith(JSON.stringify({failure: `no manga found with name ${manga}`}));
 
-  // const selectedChapter = mangaRecord.chapters[chapter];
-  // if(!selectedChapter) return respondWith(JSON.stringify({failure: `${manga} does not have a chapter ${chapter}`}));
-  // if(!userSettings[manga]) userSettings[manga] = {};
-  // userSettings[manga].chapter = chapter;
+  const selectedChapter = mangaRecord.chapters[chapter];
+  if(!selectedChapter) return respondWith(JSON.stringify({failure: `${manga} does not have a chapter ${chapter}`}));
+  if(!userSettings[manga]) userSettings[manga] = {};
+  userSettings[manga].chapter = chapter;
   
-  // await data.set({table: "users", key: userid, data: JSON.stringify(userSettings)}).catch(err => err)
-  // return respondWith(JSON.stringify(selectedChapter));
-  return respondWith(JSON.stringify({testing: "buggin"}))
+  await data.set({table: "users", key: userid, data: JSON.stringify(userSettings)}).catch(err => err)
+  return respondWith(JSON.stringify(selectedChapter));
 }
 
 function respondWith(msg) {
