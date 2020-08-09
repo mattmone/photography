d90367a16a29692fbedd868555e1071e35879c34
Matthew Mone <mmone@vernier.com>
2020-08-09 14:35:55 -0700

    return selected chapter

--- src/http/get-u-000id-get-000m-chapter-000c/index.js
+++ src/http/get-u-000id-get-000m-chapter-000c/index.js
@@ -6,15 +6,15 @@ exports.handler = async function http (req) {
   const { id: userid, m: manga, c: chapter } = req.pathParameters;
   const userSettings = (await data.get({table: 'users', key: userid})) || {};
   const mangaRecord = (await data.get({table: 'manga', key: manga}));
-  if(!mangaRecord) return respondWith(JSON.stringify({failure: `no manga found with name ${manga}`}))
+  if(!mangaRecord) return respondWith(JSON.stringify({failure: `no manga found with name ${manga}`}));
 
   const selectedChapter = mangaRecord.chapters[chapter];
-  if(!selectedChapter) return respondWith(JSON.stringify({failure: ``}))
+  if(!selectedChapter) return respondWith(JSON.stringify({failure: `${manga} does not have a chapter ${chapter}`}));
   if(!userSettings[manga]) userSettings[manga] = {};
   userSettings[manga].chapter = chapter;
   
   await data.set({table: "users", key: userid, data: JSON.stringify(userSettings)}).catch(err => err)
-  return respondWith(JSON.stringify(chapter));
+  return respondWith(JSON.stringify(selectedChapter));
 }
 
 function respondWith(msg) {
